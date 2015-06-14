101ca67a24accdfd580564aeb9382911ac8044b7
Francois Billant <fbillant@gmail.com>
2015-06-14 23:05:02 +0000

    flake8 - tests/utils_tests.py

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -1,10 +1,9 @@
-from nose.tools import *
+from nose.tools import assert_equal
 import unittest
 import autodl.utils
 import autodl.settings
 import os
 import json
-
 
 
 class TestGetWebpage(unittest.TestCase):
@@ -21,38 +20,47 @@ class TestGetWebpage(unittest.TestCase):
 
     def test_get_webpage(self):
         raw_page = autodl.utils.get_webpage("/tmp/testpage.html")
-    
+
         assert_equal(raw_page, '<html><body>yeah</body></html>')
         assert isinstance(raw_page, str)
         with self.assertRaises(IOError):
-            autodl.utils.get_webpage("/tmp/errorpage.html")    
-    
-class TestGetActivatedHosters(unittest.TestCase):    
+            autodl.utils.get_webpage("/tmp/errorpage.html")
+
+
+class TestGetActivatedHosters(unittest.TestCase):
 
     def setUp(self):
         with open("/tmp/config.json", "w") as file_tmp:
-            json.dump({"hosters":["uplea", "1fichier", "Filefactory", "tuslfile"], "supported_sites":{"animes":["mangaFrCom", "animeserv"], "series":["scenesources", "telechargementz"]}, "target_titles":{"animes":{"naruto": "None", "shoukugeki": "9"}, "series":{"atlantis": "10", "olympus": "None"}},"target_ep":{"shoukugeki": "9","atlantis": "10"}}, file_tmp)
+            json.dump({"hosters": ["uplea", "1fichier", "Filefactory",
+                       "tuslfile"], "supported_sites": {"animes":
+                       ["mangaFrCom", "animeserv"], "series":
+                       ["scenesources", "telechargementz"]}, "target_titles":
+                       {"animes": {"naruto": "None", "shoukugeki": "9"},
+                       "series": {"atlantis": "10", "olympus": "None"}},
+                       "target_ep": {"shoukugeki": "9", "atlantis": "10"}},
+                      file_tmp)
 
     def tearDown(self):
         os.remove("/tmp/config.json")
 
     def test_get_activated_hosters(self):
-        autodl.settings.set_globals({"CONFIG_FILE":"/tmp/config.json"})
+        autodl.settings.set_globals({"CONFIG_FILE": "/tmp/config.json"})
         # test the if condition
-        hosters = autodl.utils.get_activated_hosters(config_file="/tmp/config.json")
-        #print hosters
+        hosters = autodl.utils.get_activated_hosters(config_file="\
+                                                     /tmp/config.json")
+        # print hosters
 
         assert isinstance(hosters, list)
-        assert_equal(hosters, [u'uplea', u'1fichier', u'Filefactory', u'tuslfile'])
+        assert_equal(hosters, [u'uplea', u'1fichier', u'Filefactory',
+                               u'tuslfile'])
 
         # test the else statement
         hosters = autodl.utils.get_activated_hosters()
-        assert_equal(hosters, [u'uplea', u'1fichier', u'Filefactory', u'tuslfile'])
+        assert_equal(hosters, [u'uplea', u'1fichier', u'Filefactory',
+                               u'tuslfile'])
 
         # test the error
         with self.assertRaises(IOError):
-            autodl.settings.set_globals({"CONFIG_FILE":"/tmp/error.json"})
+            autodl.settings.set_globals({"CONFIG_FILE": "/tmp/error.json"})
             hosters = autodl.utils.get_activated_hosters()
             print hosters
-
-        
